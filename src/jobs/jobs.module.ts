23cfef11d4880aac0a46eb50e5bc14594b74a319
Ibra Nurlette <sjsdev02@gmail.com>
2021-02-08 13:12:33 +0700

    middleware

--- src/jobs/jobs.module.ts
+++ src/jobs/jobs.module.ts
@@ -1,12 +1,24 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JobsController } from './jobs.controller';
 import { JobsService } from './jobs.service';
 import { JobSchema } from './schemas/job.schema';
+import { AuditMiddleware } from 'src/middlewares/audit.middleware';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'Job', schema: JobSchema }])],
   controllers: [JobsController],
   providers: [JobsService],
 })
-export class JobsModule {}
+export class JobsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuditMiddleware)
+      .forRoutes({ path: 'jobs/*', method: RequestMethod.DELETE });
+  }
+}
---
+++ src/middlewares/audit.middleware.ts
@@ -0,0 +1,14 @@
+import { NestMiddleware, Injectable } from '@nestjs/common';
+
+import { Request } from 'express-serve-static-core';
+import { Response } from 'express';
+
+@Injectable()
+export class AuditMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: Function) {
+    console.log('Logging Delete request IP', req.ip);
+    console.log('Logging Delete request Path', req.path);
+    console.log('Logging Delete request Headers', req.headers);
+    next();
+  }
+}
